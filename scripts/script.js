276bf2784cc4ccf23da5d11a0433d0ad379f0b02
Braeden <braeden.craig@gmail.com>
2019-02-21 15:20:22 -0500

    updated JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,59 +1,43 @@
-// //SAMPLE CALLS //
-// // Call to get track ID
-// $.ajax({
-//     url: 'https://api.musixmatch.com/ws/1.1/track.search',
-//     method: 'GET',
-//     dataType: 'jsonp',
-//     data: {
-//         apikey: '004fc1c5222f94cf5c07c80c81fa2f62',
-//         format: 'jsonp',
-//         q_track: 'Bohemian Rhapsody',
-//         q_artist: 'Queen',
-//         f_has_lyrics: '1',
-//         s_track_rating: 'desc'
-//     }
-// }).then(function (response) {
-//     console.log(response);
-//     //get ID from response
-// });
-
-// //second call to get lyrics
-// $.ajax({
-//     url: 'https://api.musixmatch.com/ws/1.1/track.lyrics.get',
-//     method: 'GET',
-//     dataType: 'jsonp',
-//     data: {
-//         apikey: '004fc1c5222f94cf5c07c80c81fa2f62',
-//         format: 'jsonp',
-//         track_id: 75968499
-//     }
-// }).then(function (results) {
-//     console.log(results);
-// });
-
 //namespace
 const app = {};
 
 app.trackUrl = 'https://api.musixmatch.com/ws/1.1/track.search';
-app.lyricUrl = 'https://api.musixmatch.com/ws/1.1/track.lyrics.get';
+app.lyricUrl = 'https://api.lyrics.ovh/v1/';
 app.apiKey = '004fc1c5222f94cf5c07c80c81fa2f62';
 
-
-//call to get track ID
+//call to get genre
 app.getTrack = $.ajax({
     url: app.trackUrl,
     method: 'GET',
     dataType: 'jsonp',
     data: {
-        apikey: app.apiKey,
-        formatL 'jsonp',
+        apiKey: app.apiKey,
+        format: 'jsonp',
         f_has_lyrics: 1,
         s_track_rating: 'desc',
         q_track: app.userTrack,
         q_artist: app.userArtist
     }
-}).then(function(response) {
-    const trackID = response.message.body.track_list[0].track.track_ID;
-    console.log(trackID);
+}).then(function (response) {
+    const app.trackGenre = response.message.body.track_list[0].track.primary_genres.music_genre_list[0].music_genre.music_genre_name;
+    console.log(app.trackGenre);
 })
+
+// call to get lyrics 
+app.getLyrics = $.ajax({
+    url: `app.lyricUrl${app.userArtist}/${app.userTrack}`,
+    method: 'GET',
+    dataType: 'jsonp'
+}).then(function (response) {
+    const app.finalLyrics = response.message.body.lyrics.lyrics_body;
+    console.log(app.finalLyrics)
+})
+
+$.when(app.getTrack)
+    .then(app.getLyrics) => {
+    console.log(app.getLyrics)
+});
+
+
+
 
