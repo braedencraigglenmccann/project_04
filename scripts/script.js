823ddb9bc1a8989706268cf5a8d92bb0c2d06cbd
Glen <glenmccann2@gmail.com>
2019-02-23 15:05:14 -0500

    added code comments for existing scripts

--- scripts/script.js
+++ scripts/script.js
@@ -5,11 +5,9 @@ app.trackUrl = 'https://api.musixmatch.com/ws/1.1/track.search';
 app.lyricUrl = 'https://api.lyrics.ovh/v1/';
 app.apiKey = '004fc1c5222f94cf5c07c80c81fa2f62';
 app.trackGenre = 'default-styles';
-// app.finalLyrics = '';
-
 
 //call to get genre
-app.getTrack = () => {
+app.getGenre = () => {
     $.ajax({
         url: app.trackUrl,
         method: 'GET',
@@ -24,12 +22,12 @@ app.getTrack = () => {
         }
     })
         .then(function (response) {
-            // if (response.message.body.track_list[0]) {
-            //     app.trackGenre = response.message.body.track_list[0].track.primary_genres.music_genre_list[0].music_genre.music_genre_name;
-            //     // console.log(app.trackGenre);
-            // } else {
-            //     app.trackGenre = 'default-styles';
-            // }
+            if (response.message.body.track_list[0]) {
+                app.trackGenre = response.message.body.track_list[0].track.primary_genres.music_genre_list[0].music_genre.music_genre_name;
+                console.log(app.trackGenre);
+            } else {
+                app.trackGenre = 'default-styles';
+            }
         });
 }
 
@@ -49,6 +47,7 @@ app.getLyrics = () => {
     app.printLyrics();
 }
 
+// function for putting the new lyrics on the page
 app.printLyrics = () => {
     $('p.lyrics').text('');
     if (app.finalLyrics !== '') {
@@ -58,6 +57,7 @@ app.printLyrics = () => {
     }
 }
 
+// functions to initiate style changes based on genre
 app.styleReset = () => {
     $('.genre-class').removeClass(`${app.trackGenre}`);
     app.trackGenre = 'default-styles';
@@ -68,23 +68,25 @@ app.newStyles = () => {
     $('.genre-class').addClass(`${app.trackGenre}`);
 }
 
+// document ready
 $(function () {
+    // 'Pick a Song' button event listening
     $('.begin-button').on('click', function () {
+        // get user search parameters from form
         app.userArtist = $('#user-artist').val();
         app.userTrack = $('#user-track').val();
 
-        // app.getTrack();
+        // make API requests for genre and lyrics based on user search
+        app.getGenre();
         app.getLyrics();
         // app.styleReset();
         // app.newStyles();
-        
-        
-        
+
+        // scroll to lyrics section
         $('.results').animatescroll();
-        
-        // app.printLyrics();
     });
 
+    // event listener for 'sing another song' button
     $('.bottom').click(function () {
         $(this).scrollTop(0);
         $('#reset').trigger('reset');
